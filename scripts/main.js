cff61d979a5d20f203f13a87103e987f572b4a91
Dale Blackburn <dakebl@users.noreply.github.com>
2017-03-29 17:53:06 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -10,18 +10,13 @@ function toggleSidebar(){
   sidebar     = document.getElementById("sidebar"),
   mainContent = document.getElementById("main-content");
 
-if( window.width >= 768 ){
-	if (sidebar.style.display == 'none') {
-		sidebar.style.display = 'block';
-		mainContent.style.marginLeft = "200px";
-	} else {
-		sidebar.style.display = 'none';
-		mainContent.style.marginLeft = "0px";
-	}
-} else
-	if (sidebar.style.display == 'none') {
-		sidebar.style.display = 'block';
-	} else {
-		sidebar.style.display = 'none';
+	if( window.width < 768 ){
+		if (sidebar.style.display == 'none') {
+			sidebar.style.display = 'block';
+			mainContent.style.marginLeft = "200px";
+		} else {
+			sidebar.style.display = 'none';
+			mainContent.style.marginLeft = "0px";
+		}
 	}
 }
