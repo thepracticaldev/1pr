e71721c7510d27dd35ada130acd9ce11da1efe42
Dale Blackburn <dakebl@users.noreply.github.com>
2017-03-29 17:48:08 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -10,11 +10,12 @@ function toggleSidebar(){
   sidebar     = document.getElementById("sidebar"),
   mainContent = document.getElementById("main-content");
 
-  if (sidebar.style.display == 'none') {
-    sidebar.style.display = 'block';
-    mainContent.style.marginLeft = "200px";
-  } else {
-    sidebar.style.display = 'none';
-    mainContent.style.marginLeft = "0px";
-  }
+	if (sidebar.style.display == 'none') {
+		sidebar.style.display = 'block';
+		mainContent.style.marginLeft = "200px";
+	} else {
+		sidebar.style.removeProperty('display');
+		mainContent.style.marginLeft = "0px";
+	}
+	
 }
