7c6dd55580a83be29c56da135b074fbbe3c5f135
Eitan Adler <lists@eitanadler.com>
2017-04-14 17:27:38 -0400

    use ci/

--- ci/install.sh
+++ ci/install.sh
@@ -3,4 +3,4 @@ set -ex
 cd "$(git rev-parse --show-toplevel)" || exit 1
 bundle install --retry 3 --gemfile="$BUNDLE_GEMFILE"
 pip install -r ci/requirements.txt
-npm install -g jshint
+npm install -g jshint htmlhint csslint
