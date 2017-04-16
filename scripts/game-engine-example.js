bd86fc1e3a379707c5190df41d08785369dab6f5
Anton Frattaroli <frattaro@gmail.com>
2017-04-15 21:31:10 -0400

    Standardized game engines, made game engines instance-able and swappable mid-play. Added example game engine (like an interface).

---
+++ scripts/game-engine-example.js
@@ -0,0 +1,29 @@
+﻿if (typeof OnePRGame === 'undefined') {
+    var OnePRGame = {};
+}
+
+// An example game engine. Defines what needs to be supported.
+// Could make, say, a text-based game or an isometric perspective,
+// in addition to the 2D and 3D currently available.
+OnePRGame.EngineExample = function (context, gameBoardClassName) {
+
+	// The map placeholder element's class name.
+    this.GameBoardClassName = gameBoardClassName;
+
+	// The map, tileset, player objects.
+    this.Context = context;
+
+	// Used for instancing.
+    let that = this;
+
+	// Start up engine. May already be started. Draw map, player, setup controls.
+    this.Run = function () { };
+
+    // Stop engine. May or may not have intervals to cancel.
+	// Callback for when stop is complete.
+    this.Stop = function (callback) { };
+
+	// Handle the player action control.
+	// Should probably be named something different. Just using it currently to display text.
+    this.PlayerAction = function (text) { };
+};
