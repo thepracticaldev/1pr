bf77d4e66bcf5fc228d8aaf089563baf04c3237d
Andy Zhao <andyzhao.zhao@gmail.com>
2018-08-13 14:41:40 -0400

    Clean up config.js

--- scripts/database/config.js
+++ scripts/database/config.js
@@ -3,10 +3,10 @@
 
 //Please overide this with the config for your test firebase project
 var config = {
-	apiKey: "AIzaSyBTsOTB3XvmDdU6RcO1DlOOV-govpkDs4E",
-	authDomain: "onepr-453d8.firebaseapp.com",
-	databaseURL: "https://onepr-453d8.firebaseio.com",
-	storageBucket: "onepr-453d8.appspot.com",
-	messagingSenderId: "240320654850"
+	apiKey: "",
+	authDomain: "",
+	databaseURL: "",
+	storageBucket: "",
+	messagingSenderId: ""
 };
 firebase.initializeApp(config);
